102a77b4a218ba5eceaf2028f17827b7537747d5
chirchil <toshashibalov@yandex.ru>
2022-05-14 09:39:03 +0300

    first commit

--- tests/functional_tests/test_index.py
+++ tests/functional_tests/test_index.py
@@ -4,7 +4,7 @@ import os
 
 from selenium.webdriver.common.by import By
 
-chrome_driver = "C:/Users/IIoT_Lab/PycharmProjects/flaskProject/tests/functional_tests/yandexdriver.exe" if "ChromeWebDriver" not in os.environ.keys() else os.path.join(
+chrome_driver = "C:/Users/tosha/Desktop/hw_pipeline/PipelinePractice/tests/functional_tests/chromedriver.exe" if "ChromeWebDriver" not in os.environ.keys() else os.path.join(
     os.environ["ChromeWebDriver"], 'chromedriver.exe')
 
 
